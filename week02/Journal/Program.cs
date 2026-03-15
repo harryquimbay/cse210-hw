9dbe4468a5f88f44384f6ada02f1f990bc5f79b5
harryquimbay <hquimbay@byupathway.edu>
2026-03-14 23:38:16 -0500

    excerw02jour

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal theJournal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        int choice = 0;
+
+        while (choice != 5)
+        {
+            Console.WriteLine("Please select one of the following choices:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+
+            string input = Console.ReadLine();
+            choice = int.Parse(input);
+            Console.WriteLine();
+
+            if (choice == 1)
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+                Console.WriteLine(prompt);
+                Console.Write("> ");
+                string response = Console.ReadLine();
+
+                Entry newEntry = new Entry();
+                newEntry._date = DateTime.Now.ToShortDateString();
+                newEntry._promptText = prompt;
+                newEntry._entryText = response;
+
+                theJournal.AddEntry(newEntry);
+            }
+            else if (choice == 2)
+            {
+                theJournal.DisplayAll();
+            }
+            else if (choice == 3)
+            {
+                Console.Write("What is the filename? ");
+                string file = Console.ReadLine();
+                theJournal.LoadFromFile(file);
+            }
+            else if (choice == 4)
+            {
+                Console.Write("What is the filename? ");
+                string file = Console.ReadLine();
+                theJournal.SaveToFile(file);
+            }
+            else if (choice == 5)
+            {
+                Console.WriteLine("Goodbye!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice.");
+            }
+
+            Console.WriteLine();
+        }
     }
 }
