0523b22c9a16bfc4959feb61387dd10bd972704c
harryquimbay <hquimbay@byupathway.edu>
2026-03-21 01:41:24 -0500

    proj03

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+    }
+}
