848b5fdf83c949a2b4810c89bdfdfbeb5417cb42
harryquimbay <hquimbay@byupathway.edu>
2026-04-15 20:55:08 -0500

    week07 final

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new RunningActivity(new DateTime(2022, 11, 3), 30, 3.0),
+            new CyclingActivity(new DateTime(2022, 11, 3), 45, 12.0),
+            new SwimmingActivity(new DateTime(2022, 11, 3), 40, 20)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
